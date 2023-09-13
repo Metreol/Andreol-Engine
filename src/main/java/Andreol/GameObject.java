7e703de70bcb5e409ebb63192e9ccddfa759826e
Metreol-XPS <project.metreol@gmail.com>
2023-09-13 21:40:51 +0100

    Added the GameObject class and the abstract Component class.

---
+++ src/main/java/Andreol/GameObject.java
@@ -0,0 +1,54 @@
+package Andreol;
+
+import java.util.List;
+
+public class GameObject {
+    private String name;
+    private List<Component> components;
+    public GameObject(String name) {
+        this.name = name;
+    }
+
+    public void start() {
+        for (Component component : components) {
+            component.start();
+        }
+    }
+
+    public void update(float deltaTime) {
+        for (Component component : components) {
+            component.update(deltaTime);
+        }
+    }
+
+    public <T extends Component> T getComponent(Class<T> componentClass) {
+        // Might need to change to normal for loops, in other methods too
+        for (Component component : components) {
+            if (componentClass.isAssignableFrom(component.getClass())) {
+                try {
+                    return componentClass.cast(component);
+                } catch (ClassCastException cce) {
+                    cce.printStackTrace();
+                    assert false : String.format("Error: (GameObject) Could not cast '%s' to '%s'.", component, componentClass);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public <T extends Component> void addComponent(Component component) {
+        components.add(component);
+        component.gameObject = this;
+    }
+
+    public <T extends Component> void removeComponent(Class<T> componentClass) {
+        for (Component component : components) {
+            if (componentClass.isAssignableFrom(component.getClass())) {
+                components.remove(component);
+                return;
+            }
+        }
+    }
+
+}
